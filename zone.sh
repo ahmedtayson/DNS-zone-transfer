efccfd28b0877f31fc2671526437a6d34760e03f
ahmed.tasyon <42816068+ahmedtayson@users.noreply.github.com>
2021-09-04 17:48:23 -0400

    Update zone.sh
    #!/bin/bash
    if [ -z "$1" ];then
    echo "[*] Usage $0 domain.com "
    exit 0
    fi
    
    for i in $(host -t ns $1 |cut -d" " -f4 |cut -d"." -f1,2,3 );do 
    dig @$i  $1 AXFR
    done

--- zone.sh
+++ zone.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-if [ -z "$1" ];then
-echo "[*] Usage $0 domain.com "
-exit 0
-fi
 
-for i in $(host -t ns $1 |cut -d" " -f4 | cut -d"." -f1,2,3 );do 
-host -l $1 $i | grep "has address"
-done
